544a66dcb00170ee193464d64b92b7640a588e29
lan4@umbc.edu <lan4@umbc.edu@a29ba32f-6ec0-86ad-cf81-a966497ee43f>
2012-04-18 01:00:16 +0000

    Screens added

---
+++ Shroud_main/Shroud/Shroud/Screens/LevelScreen.cs
@@ -0,0 +1,289 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FlatRedBall;
+using FlatRedBall.Math.Geometry;
+
+using Shroud.Utilities;
+using Shroud.Entities;
+using System.Text.RegularExpressions;
+
+namespace Shroud.Screens
+{
+    public class LevelScreen : Screen
+    {
+        private Sprite mBg;
+        private Sprite mTitle;
+        private List<LevelButton> mLevels;
+        private BackButton mBack;
+
+        private class LevelButton : PositionedObject
+        {
+            private Sprite mbg;
+            private Sprite mFace;
+            private Sprite mDone;
+            private Sprite mBadge1;
+            private Sprite mBadge2;
+            private Sprite mBadge3;
+
+            private string mLevelString;
+
+            public string LevelString
+            {
+                get { return mLevelString; }
+            }
+
+            private AxisAlignedRectangle mCollision;
+
+            public AxisAlignedRectangle Collision
+            {
+                get { return mCollision; }
+            }
+
+            public LevelButton(string tags) : base()
+            {
+                mbg = SpriteManager.AddSprite(@"Content/Menus/Levels/scrollBase", "Global");
+                mbg.AttachTo(this, false);
+                GameProperties.RescaleSprite(mbg);
+                mbg.RelativeRotationZ = GameProperties.WorldRotation;
+
+                string face;
+
+                if (tags.Contains('g'))
+                {
+                    face = "general";
+                }
+                else
+                {
+                    face = "target";
+                }
+
+                mFace = SpriteManager.AddSprite(@"Content/Menus/Levels/" + face, "Global");
+                mFace.AttachTo(this, false);
+                GameProperties.RescaleSprite(mFace);
+                mFace.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mDone = SpriteManager.AddSprite(@"Content/Menus/Levels/crossedOut", "Global");
+                mDone.AttachTo(this, false);
+                GameProperties.RescaleSprite(mDone);
+                mDone.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mBadge1 = SpriteManager.AddSprite(@"Content/Menus/Levels/notSeenBadge", "Global");
+                mBadge1.AttachTo(this, false);
+                GameProperties.RescaleSprite(mBadge1);
+                mBadge1.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mBadge2 = SpriteManager.AddSprite(@"Content/Menus/Levels/killedOnlyTargetBadge", "Global");
+                mBadge2.AttachTo(this, false);
+                GameProperties.RescaleSprite(mBadge2);
+                mBadge2.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mBadge3 = SpriteManager.AddSprite(@"Content/Menus/Levels/didNotDieBadge", "Global");
+                mBadge3.AttachTo(this, false);
+                GameProperties.RescaleSprite(mBadge3);
+                mBadge3.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mBadge1.Visible = tags.Contains('a');
+                mBadge2.Visible = tags.Contains('b');
+                mBadge3.Visible = tags.Contains('c');
+                mDone.Visible = tags.Contains('d');
+
+                mCollision = ShapeManager.AddAxisAlignedRectangle();
+                mCollision.ScaleX = mbg.ScaleY;
+                mCollision.ScaleY = mbg.ScaleX;
+                mCollision.AttachTo(this, false);
+                mCollision.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mLevelString = "level" + Regex.Match(tags, @"\d+").Value + ".txt";
+            }
+
+            public void Destroy()
+            {
+                SpriteManager.RemovePositionedObject(this);
+                SpriteManager.RemoveSprite(mbg);
+                SpriteManager.RemoveSprite(mFace);
+                SpriteManager.RemoveSprite(mDone);
+                SpriteManager.RemoveSprite(mBadge1);
+                SpriteManager.RemoveSprite(mBadge2);
+                SpriteManager.RemoveSprite(mBadge3);
+                ShapeManager.Remove(mCollision);
+            }
+        }
+
+        private class BackButton : PositionedObject
+        {
+            private AxisAlignedRectangle mCollision;
+            private Sprite mButton;
+
+            public AxisAlignedRectangle Collision
+            {
+                get { return mCollision; }
+            }
+
+            public BackButton(string contentManagerName)
+                : base()
+            {
+                SpriteManager.AddPositionedObject(this);
+
+                mButton = SpriteManager.AddSprite(@"Content/Menus/goBack", contentManagerName);
+                mButton.AttachTo(this, false);
+                GameProperties.RescaleSprite(mButton);
+                mButton.RelativeRotationZ = GameProperties.WorldRotation;
+
+                mCollision = ShapeManager.AddAxisAlignedRectangle();
+                mCollision.ScaleX = mButton.ScaleY;
+                mCollision.ScaleY = mButton.ScaleX;
+                mCollision.AttachTo(this, false);
+            }
+
+            public void Destroy()
+            {
+                SpriteManager.RemovePositionedObject(this);
+                SpriteManager.RemoveSprite(mButton);
+                ShapeManager.Remove(mCollision);
+            }
+        }
+
+        #region Methods
+        
+        #region Constructor and Initialize
+
+        public LevelScreen()
+            : base("$safeitemname$")
+        {
+            // Don't put initialization code here, do it in
+            // the Initialize method below
+            //   |   |   |   |   |   |   |   |   |   |   |
+            //   |   |   |   |   |   |   |   |   |   |   |
+            //   V   V   V   V   V   V   V   V   V   V   V
+
+        }
+
+        public override void Initialize(bool addToManagers)
+        {
+            // Set the screen up here instead of in the Constructor to avoid
+            // exceptions occurring during the constructor.
+
+            mLevels = new List<LevelButton>();
+
+            mBg = SpriteManager.AddSprite(@"Content/Menus/bg", ContentManagerName);
+            GameProperties.RescaleSprite(mBg);
+            mBg.RotationZ = GameProperties.WorldRotation;
+
+            mTitle = SpriteManager.AddSprite(@"Content/Menus/lvlSelect", ContentManagerName);
+            GameProperties.RescaleSprite(mTitle);
+            mTitle.RotationZ = GameProperties.WorldRotation;
+            mTitle.X = 7.0f;
+
+            mBack = new BackButton(ContentManagerName);
+            mBack.X = -7.0f;
+            mBack.Y = 8.0f;
+			
+			string[] levelStrings = GameProperties.ProfileString.Split(' ');
+
+            foreach (string level in levelStrings)
+            {
+                if (level.Contains('l'))
+                {
+                    mLevels.Add(new LevelButton(level));
+                }
+            }
+
+            FormatLevels();
+
+            GestureManager.Initialize2();
+			
+			// AddToManagers should be called LAST in this method:
+			if(addToManagers)
+			{
+				AddToManagers();
+			}
+        }
+
+		public override void AddToManagers()
+        {
+		
+		
+		}
+
+        private void FormatLevels()
+        {
+            int i = 0;
+            for (int y = 2; y > -5; y -= 8)
+            {
+                for (int x = 10; x > -11; x -= 10)
+                {
+                    if (i < mLevels.Count)
+                    {
+                        mLevels[i].X = y;
+                        mLevels[i].Y = x;
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                    i++;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Activity(bool firstTimeCalled)
+        {
+            base.Activity(firstTimeCalled);
+
+            GestureManager.Update2(0.0f, 0.0f);
+
+            if (GestureManager.CurGesture == GestureManager.Gesture.Tap)
+            {
+                float x = GestureManager.EndTouchWorld.X;
+                float y = GestureManager.EndTouchWorld.Y;
+
+                foreach (LevelButton b in mLevels)
+                {
+                    if (b.Collision.IsPointInside(x, y))
+                    {
+                        GameProperties.LevelString = b.LevelString;
+                        Destroy();
+                        MoveToScreen(typeof(Screens.GameScreen).FullName);
+                    }
+                }
+
+                if (mBack.Collision.IsPointInside(x, y))
+                {
+                    Destroy();
+                    MoveToScreen(typeof(Screens.ProfileScreen).FullName);
+                }
+            }
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+
+            SpriteManager.RemoveSprite(mBg);
+            SpriteManager.RemoveSprite(mTitle);
+
+            foreach (LevelButton b in mLevels)
+            {
+                b.Destroy();
+            }
+
+            mBack.Destroy();
+
+            GestureManager.Clean();
+        }
+
+        #endregion
+
+		
+        #endregion
+    }
+}
+
---
+++ Shroud_main/Shroud/Shroud/Screens/StartScreen.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+using FlatRedBall;
+
+using Shroud.Utilities;
+using Gesture = Shroud.Utilities.GestureManager.Gesture;
+
+namespace Shroud.Screens
+{
+    public class StartScreen : Screen
+    {
+        private Sprite mBg;
+        private Sprite mLogo;
+        private Sprite mClickText;
+
+        #region Methods
+
+        #region Constructor and Initialize
+
+        public StartScreen()
+            : base("StartScreen")
+        {
+            // Don't put initialization code here, do it in
+            // the Initialize method below
+            //   |   |   |   |   |   |   |   |   |   |   |
+            //   |   |   |   |   |   |   |   |   |   |   |
+            //   V   V   V   V   V   V   V   V   V   V   V
+
+        }
+
+        public override void Initialize(bool addToManagers)
+        {
+            // Set the screen up here instead of in the Constructor to avoid
+            // exceptions occurring during the constructor.
+
+            mBg = SpriteManager.AddSprite(@"Content/Menus/bg", "Global");
+            mLogo = SpriteManager.AddSprite(@"Content/Menus/logo", "Global");
+            mClickText = SpriteManager.AddSprite(@"Content/Menus/clickScreenBtn", "Global");
+
+            GameProperties.RescaleSprite(mBg);
+            GameProperties.RescaleSprite(mLogo);
+            GameProperties.RescaleSprite(mClickText);
+
+            mBg.RotationZ = GameProperties.WorldRotation;
+            mLogo.RotationZ = GameProperties.WorldRotation;
+            mClickText.RotationZ = GameProperties.WorldRotation;
+
+            InitializeManagers();
+            IsolatedStorageFile myIsolatedStorage = IsolatedStorageFile.GetUserStoreForApplication();
+
+            if (!myIsolatedStorage.FileExists("profiles2.txt"))
+            {
+                myIsolatedStorage.Dispose();
+                GameProperties.CreateProfiles();
+            }
+            //GameProperties.Save();
+            //System.Diagnostics.Debug.WriteLine(GameProperties.Read());
+
+			// AddToManagers should be called LAST in this method:
+			if(addToManagers)
+			{
+				AddToManagers();
+			}
+        }
+
+		public override void AddToManagers()
+        {
+		
+		
+		}
+		
+        #endregion
+
+        private void Fade()
+        {
+            if (mClickText.Alpha >= 1.0f)
+            {
+                mClickText.AlphaRate = -1.0f;
+            }
+            else if (mClickText.Alpha <= 0.0f)
+            {
+                mClickText.AlphaRate = 1.0f;
+            }
+        }
+
+        private void InitializeManagers()
+        {
+            GestureManager.Initialize2();
+        }
+
+        #region Public Methods
+
+        public override void Activity(bool firstTimeCalled)
+        {
+            base.Activity(firstTimeCalled);
+
+            Fade();
+            GestureManager.Update2(0.0f, 0.0f);
+
+            if (GestureManager.CurGesture == Gesture.Tap)
+            {
+                Destroy();
+                MoveToScreen(typeof(Screens.ProfileScreen).FullName);
+            }
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+
+            SpriteManager.RemoveSprite(mBg);
+            SpriteManager.RemoveSprite(mLogo);
+            SpriteManager.RemoveSprite(mClickText);
+
+            GestureManager.Clean();
+        }
+
+        #endregion
+
+		
+        #endregion
+    }
+}
